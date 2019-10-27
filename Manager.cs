e3cf7c70d55e483f742cf2ea24d35528fb0f2016
alinnOK <alinen0ok@yandex.ru>
2019-10-27 13:02:20 +0300

    initial commit

---
+++ Manager.cs
@@ -0,0 +1,11 @@
+using System;
+namespace lesson2
+{
+public class Manager
+{
+
+    public string Name {get; set;}
+
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Manager;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager=new Manager();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
